2a33e2d801477ac810bfd2d97df790b20a61d162
Moh Faiz <fmohd9870@gmail.com>
2021-11-15 20:08:46 +0530

    xx

--- application/views/student/studentsLeavededit.php
+++ application/views/student/studentsLeavededit.php
@@ -14,12 +14,12 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="box box-primary">
-                <form action="<?php /* echo site_url("admin/show_all_student/leavedstudentsave/" *. $id) */?>" name="employeeform" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+                    <form action="<?php /* echo site_url("admin/show_all_student/leavedstudentsave/" *. $id) */ ?>" name="employeeform" method="post" accept-charset="utf-8" enctype="multipart/form-data">
                         <div class="box-body">
                             <div class="tshadow mb25 bozero">
                                 <h3 class="pagetitleh2"> <?php echo $this->lang->line('edit'); ?> <?php echo $this->lang->line('student'); ?></h3>
                                 <div class="around10">
-                                    <?php if ($this->session->flashdata('msg')) {?>
+                                    <?php if ($this->session->flashdata('msg')) { ?>
                                         <?php echo $this->session->flashdata('msg') ?>
                                     <?php }?>
                                     <?php foreach($student as $st){ ?>
@@ -45,11 +45,22 @@
                                                 <input id="current_phone" name="current_phone" placeholder="" type="number" class="form-control" required value=<?php if($st['mobileno']!="") echo $st['mobileno']; else echo "&nbsp;" ;?> />
                                             </div>
                                         </div>
-                                    
-                                        <div class="col-md-3">
-                                            <div class="form-group">
-                                                <label for="occupation">Occupation</label><small class="req"> *</small>
-                                                <input id="occupation" name="occupation" placeholder="" type="text" class="form-control"  value="" />
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="col-md-3">
+                                                    <div class="form-group">
+                                                        <label for="current_phone">Current Phone</label><small class="req"> *</small>
+                                                        <input id="current_phone" class="form-control" type="tel" name="current_phone" placeholder="012-345-6789" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required value=<?php echo (!empty($st['mobileno'])) ? $st['mobileno'] : " "; ?>>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-3">
+                                                    <div class="form-group">
+                                                        <label for="occupation">Occupation</label><small class="req"> *</small>
+                                                        <input id="occupation" name="occupation" placeholder="" type="text" class="form-control" value="" />
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4"></div>
                                             </div>
                                         </div>
                                         </div>
@@ -63,7 +74,6 @@
                                                 <!-- <input id="reason" name="reason" placeholder="" type="text" class="form-control"  value="" required/> -->
                                                 <textarea id="address" name="reason" rows="4" ></textarea>
                                             </div>
-                                        </div>
 
                                         <div class="col-md-6">
                                             <div class="form-group">
@@ -76,10 +86,14 @@
                                     </div>
                                     </div>
 
-                                        <div class="col-md-3" style="display:none;">
-                                            <div class="form-group">
-                                                <label for="name">*Name</label><small class="req"> *</small>
-                                                <input id="name" name="name" placeholder="" type="text" class="form-control"  value=<?php echo $st['firstname']."&nbsp;".$st['lastname']; ?> required/>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="col-md-3"></div>
+                                                <div class="col-md-3">
+                                                    <input name="submit" style="float: right;" placeholder="" type="submit" class="btn btn-info" value="Submit" />
+                                                </div>
+                                                <div class="col-md-6"></div>
                                             </div>
                                         </div>
                                         
@@ -96,7 +110,7 @@
                                 </div>
                             </div>
                         </div>
-                </form>
+                    </form>
                 </div>
             </div>
         </div>
