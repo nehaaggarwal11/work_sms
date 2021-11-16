d743c7c133ed2919c1eaec5c0e91e30edbdeec37
Gursharan Singh <gursharan.smartdesignhut@gmail.com>
2021-11-16 09:46:34 +0530

    stash

--- application/views/student/studentsLeavededit.php
+++ application/views/student/studentsLeavededit.php
@@ -1,8 +1,3 @@
-<style>
-    textarea{
-        width: 100%;
-    }
-</style>
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
@@ -21,28 +16,25 @@
                                 <div class="around10">
                                     <?php if ($this->session->flashdata('msg')) { ?>
                                         <?php echo $this->session->flashdata('msg') ?>
-                                    <?php }?>
-                                    <?php foreach($student as $st){ ?>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                        <div class="col-md-3">
-                                            <div class="form-group">
-                                            <label for="current_email">Current <?php echo $this->lang->line('email'); ?></label><small class="req"> *</small>
-                                                <input id="current_email" name="current_email" placeholder="" type="email" class="form-control" required value=<?php if($st['email']!="") echo $st['email']; else echo "Email" ;?> />
-                                                
-                                            </div>
-                                        </div>
-                                        
-                                        <div class="col-md-3">
-                                            <div class="form-group">
-                                            <label for="created_at">Leave Date</label><small class="req"> *</small>
-                                            <input id="created_at" name="created_at" placeholder="" type="text" class="form-control date" required value="<?php echo set_value('created_at'); ?>" />
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <div class="form-group">
-                                                <label for="current_phone">Current Phone</label><small class="req"> *</small>
-                                                <input id="current_phone" name="current_phone" placeholder="" type="number" class="form-control" required value=<?php if($st['mobileno']!="") echo $st['mobileno']; else echo "&nbsp;" ;?> />
+                                    <?php } ?>
+                                    <?php foreach ($student as $st) { ?>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="col-md-3">
+                                                    <div class="form-group">
+                                                        <label for="current_email">Current <?php echo $this->lang->line('email'); ?></label><small class="req"> *</small>
+                                                        <input id="current_email" name="current_email" placeholder="" type="email" class="form-control" required value=<?php if ($st['email'] != "") echo $st['email'];
+                                                                                                                                                                        else echo "Email"; ?> />
+
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-3">
+                                                    <div class="form-group">
+                                                        <label for="created_at">Leave Date</label><small class="req"> *</small>
+                                                        <input id="created_at" name="created_at" placeholder="" type="text" class="form-control date" required value="<?php echo set_value('created_at'); ?>" />
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="row">
@@ -62,29 +54,33 @@
                                                 </div>
                                                 <div class="col-md-4"></div>
                                             </div>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="row">
-                                        <div  class="col-md-12">
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label for="reason">*Reason</label><small class="req"> *</small>
-                                                <!-- <input id="reason" name="reason" placeholder="" type="text" class="form-control"  value="" required/> -->
-                                                <textarea id="address" name="reason" rows="4" ></textarea>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="col-md-3">
+                                                        <div class="form-group">
+                                                            <label for="reason">*Reason</label><small class="req"> *</small>
+                                                            <input id="reason" name="reason" placeholder="" type="text" class="form-control" value="" required />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-3">
+                                                        <div class="form-group">
+                                                            <label for="address">Current Address</label><small class="req"> *</small>
+                                                            <!-- <input id="address" name="address" placeholder="" type="text" class="form-control"  value=<?php /*if($st['current_address']!="") echo $st['current_address']; else echo "&nbsp;" ;*/ ?> /> -->
+                                                            <textarea id="address" name="address" rows="4" cols="42"><?php if ($st['current_address'] != "") echo $st['current_address'];
+                                                                                                                        else echo "&nbsp;"; ?></textarea>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4"></div>
+                                                </div>
                                             </div>
 
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label for="address">Current Address</label><small class="req"> *</small>
-                                                <!-- <input id="address" name="address" placeholder="" type="text" class="form-control"  value=<?php /*if($st['current_address']!="") echo $st['current_address']; else echo "&nbsp;" ;*/?> /> -->
-                                                <textarea id="address" name="address" rows="4" ><?php if($st['current_address']!="") echo $st['current_address']; else echo "&nbsp;" ;?></textarea>
+                                            <div class="col-md-3" style="display:none;">
+                                                <div class="form-group">
+                                                    <label for="name">*Name</label><small class="req"> *</small>
+                                                    <input id="name" name="name" placeholder="" type="text" class="form-control" value=<?php echo $st['firstname'] . "&nbsp;" . $st['lastname']; ?> required />
+                                                </div>
                                             </div>
-                                        </div>
-                                        
-                                    </div>
-                                    </div>
 
                                         </div>
                                         <div class="row">
@@ -96,16 +92,6 @@
                                                 <div class="col-md-6"></div>
                                             </div>
                                         </div>
-                                        
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="col-md-9"></div><div class="col-md-3">
-                                    <input name="submit" style="float: right;" placeholder="" type="submit" class="btn btn-info"  value="Submit" />
-                                    </div>
-                                      
-                                </div>
-                                    </div>
                                     <?php } ?>
                                 </div>
                             </div>
